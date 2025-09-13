56a6630517798530a1b5f5e312c60c09f122029b
mziyad10 <mvsiyad10@gmail.com>
2025-09-13 19:44:58 +0530

    feat:customized tab icons and active states

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,24 @@
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function tabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="login" options={{ title: "Login" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({color}) => <FontAwesome name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: "Login",
+          tabBarIcon: ({color}) => <AntDesign name="login" size={24} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
