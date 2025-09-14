b3bba7239ddab4865e59390148848758ab454729
mziyad10 <mvsiyad10@gmail.com>
2025-09-14 13:17:36 +0530

    feat:implemented RouteGuard and Sign in/Sign up functions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect } from "react";
+
+function RouteGuard({children} : {children:React.ReactNode}){
+  const router = useRouter()
+  const isAuth = false;
+
+  useEffect(()=>{
+    if(!isAuth){
+      router.replace("/login")
+    }
+  })
+  return<>{children}</>
+}
 
 export default function RootLayout() {
   return (
+    <RouteGuard>
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown:false }} />
     </Stack>
+    </RouteGuard>
   );
 }
