f22909123fde065c008a5039332f3d6ad95dad68
mziyad10 <mvsiyad10@gmail.com>
2025-09-18 21:34:17 +0530

    implemented Auth provider and redirecting logged-in users

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/lib/auth-context";
 import { Stack, useRouter } from "expo-router";
 import React, { useEffect } from "react";
 
@@ -15,10 +16,12 @@ function RouteGuard({children} : {children:React.ReactNode}){
 
 export default function RootLayout() {
   return (
+    <AuthProvider>
     <RouteGuard>
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown:false }} />
     </Stack>
     </RouteGuard>
+    </AuthProvider>
   );
 }
--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -1,6 +1,49 @@
-import React from "react";
+import { createContext, useContext } from "react";
+import { ID, Models } from "react-native-appwrite";
+import { account } from "./appwrite";
 
+const AuthContextType = {
+    // user: Models.User<Models.Preferences> | null;
+    // isLoadingUser: boolean;
+    signUp: (email: string, password: string) => Promise<string | null>;
+    signIn: (email: string, password: string) => Promise<string | null>;
+    signOut: () => Promise<void>;
+}
 
-export function authProvider({children}:{children:React.ReactNode}){
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+export function AuthProvider({children}:{children:React.ReactNode}){
+
+    const signUp = async(email: string, password: string)=>{
+        try {
+            await account.create(ID.unique(),email, password)
+            await signIn(email, password)
+            return null
+        } catch (error) {
+            if(error instanceof Error){
+                return error.message
+            }
+            return "An error occured during signUp"
+        }
+    }
+
+    const signIn = async(email: string, password: string)=>{
+        try {
+            await account.createEmailPasswordSession(email, password)
+            return null
+        } catch (error) {
+            if(error instanceof Error){
+                return error.message
+            }
+            return "An error occured during signIn"
+        }
+    }
+
+return <AuthContext.Provider value={{ signUp, signIn}}>
+    {children}
+</AuthContext.Provider>
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext)
 }
