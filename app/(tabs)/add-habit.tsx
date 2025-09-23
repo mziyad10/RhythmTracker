4cf9eed52b6345c3b73aa82ea8dff80172ba7353
mziyad10 <mvsiyad10@gmail.com>
2025-09-23 20:59:28 +0530

    feat:completed building add-habit screen UI

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,9 +1,55 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
+
+const FREQUENCIES = ["daily", "weekly", "monthly"];
+
+type Frequency = (typeof FREQUENCIES)[number];
 
 export default function AddHabitScreen() {
+  const [title, setTitle] = useState<String>("");
+  const [description, setDescription] = useState<String>("");
+  const [frequency, setFrequency] = useState<String>("daily");
+
   return (
-    <View>
-      <Text> Hello login page</Text>
+    <View style={styles.container}>
+      <TextInput
+        label="Title"
+        mode="outlined"
+        onChangeText={setTitle}
+        style={styles.input}
+      />
+      <TextInput
+        label="Description"
+        mode="outlined"
+        onChangeText={setDescription}
+        style={styles.input}
+      />
+      <View style={styles.frequencyContainer}>
+        <SegmentedButtons
+        value={frequency}
+        onValueChange={(value)=>setFrequency(value as Frequency)}
+          buttons={FREQUENCIES.map((freq) => ({
+            value: freq,
+            label: freq.charAt(0).toUpperCase() + freq.slice(1),
+          }))}
+        />
+      </View>
+      <Button mode="contained" disabled={!title || !description}>Add Habit</Button>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 16,
+  },
+  frequencyContainer: {
+    marginBottom: 24,
+  },
+});
